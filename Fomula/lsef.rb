c735aa96a075d312ce0ece87f8199af7d1630929
Takayuki Todo <i2686172@cc.kyoto-su.ac.jp>
2026-07-03 12:19:12 +0900

    Update lsef.rb

--- Fomula/lsef.rb
+++ Fomula/lsef.rb
@@ -6,8 +6,16 @@ class Lsef < Formula
   version VERSION
   license "MIT"
 
-  url "https://github.com/Takayuki-Todo/lsef/releases/download/v#{VERSION}/lsef-#{VERSION}_darwin_amd64.tar.gz"
-  sha256 "ae16f0822e51ed26090578a532d29c79fe6f226a42edef195bfbb31fb87ecf8e"
+  if OS.mac? && Hardware::CPU.intel?
+    url "https://github.com/Takayuki-Todo/lsef/releases/download/v#{VERSION}/lsef-#{VERSION}_darwin_amd64.tar.gz"
+    sha256 "ae16f0822e51ed26090578a532d29c79fe6f226a42edef195bfbb31fb87ecf8e"
+  end
+
+  if OS.mac? && Hardware::CPU.arm?
+    url "https://github.com/Takayuki-Todo/lsef/releases/download/v#{VERSION}/lsef-#{VERSION}_darwin_arm64.tar.gz"
+    sha256 "b00895e5b0379fa02f0e816112bc96ff1d9219d9854cefd6005777231292920a"
+  end
+
   def install
     bin.install "lsef"
     bash_completion.install "completions/bash/lsef"
