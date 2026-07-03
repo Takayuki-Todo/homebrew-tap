41ce7edbbed5f1867b2a6c5758cfcd94a0241abf
Takayuki Todo <i2686172@cc.kyoto-su.ac.jp>
2026-07-03 11:53:51 +0900

    Fix lsef checksum

--- Fomula/lsef.rb
+++ Fomula/lsef.rb
@@ -8,7 +8,6 @@ class Lsef < Formula
 
   url "https://github.com/Takayuki-Todo/lsef/releases/download/v#{VERSION}/lsef-#{VERSION}_darwin_amd64.tar.gz"
   sha256 "ae16f0822e51ed26090578a532d29c79fe6f226a42edef195bfbb31fb87ecf8e"
-
   def install
     bin.install "lsef"
     bash_completion.install "completions/bash/lsef"
